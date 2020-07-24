9bda22791fb315125eb7cdfe6f8a5eee7d6f7b9c
Darshan Sharma <thedarshansharma@gmail.com>
2020-07-24 09:20:14 +0530

    removed firebase keys

--- src/client/app.ts
+++ src/client/app.ts
@@ -5,14 +5,7 @@ class InitializeFirebase {
     constructor() {
         // console.log('admin - ', admin);
         var firebaseConfig = {
-            apiKey: "AIzaSyChdiU0uyXYye-bk8aRYqT1lqJFWeTZm_s",
-            authDomain: "auth-24107.firebaseapp.com",
-            databaseURL: "https://auth-24107.firebaseio.com",
-            projectId: "auth-24107",
-            storageBucket: "auth-24107.appspot.com",
-            messagingSenderId: "512747461237",
-            appId: "1:512747461237:web:3e83acf34bfc188e36363c",
-            measurementId: "G-YKL3KFXBKL"
+            // Take it from firebase.com
         };
         // Initialize Firebase
         firebase.initializeApp(firebaseConfig);
